556fdda1f676d94fef26dc2a3c65b85ad6bc7db4
Fabian, Peter <Peter.Fabian@cdk.com>
2016-04-05 13:23:17 -0700

    update print output for getUserAltInfo

--- src/getUserAltInfo.rb
+++ src/getUserAltInfo.rb
@@ -9,6 +9,7 @@ class GetAltNumberInfo
 	def get_users_and_alt_nums
 
 		cmd_ok,user_list = $bw.get_users_in_group(@ent,@group)
+		counter = 0
 		user_list.each do |user|
 			@users_info[user] = Array.new
 			puts "Checking user; #{user}"
@@ -16,14 +17,13 @@ class GetAltNumberInfo
 			[:lastName,:firstName,:phoneNumber,:extension].each { |ele| @users_info[user].push(config_hash[ele]) }
 
 			cmd_ok,svc_list = $bw.get_user_svc_list(user)
-			@users_info[user].push(get_alt_assignment(svc_list,user))
+			get_alt_assignment(svc_list,user)
+			counter += 1
 		end
 		print_users_info
 	end
 
 	def get_alt_assignment(svc_list,user)
-        alt_nums_list = Array.new
-
 		svc_list.each do |svc_hash|
 			if svc_hash[:Service_Name] == "Alternate Numbers" and svc_hash[:Assigned] == "true"
 				cmd_ok,config_hash = $bw.get_user_alternate_numbers(user)
@@ -34,38 +34,34 @@ class GetAltNumberInfo
 		            altNum = alt_num_string.to_sym
 		            if config_hash.has_key?(altNum)
 		            	if config_hash[altNum].has_key?(:phoneNumber)
-		            		alt_nums_list.push(config_hash[altNum][:phoneNumber])
+		            		@users_info[user].push(config_hash[altNum][:phoneNumber])
 		            	elsif config_hash[altNum].has_key?(:extension)
-		            		alt_nums_list.push(config_hash[altNum][:extension])
+		            		@users_info[user].push(config_hash[altNum][:extension])
 		            	else
-		            		alt_nums_list.push("EMPTY")
+		            		@users_info[user].push("EMPTY")
 		        		end
 					else
-			           alt_nums_list.push("EMPTY")	 		            		
+	            		@users_info[user].push("EMPTY")
+
 		            end
 		            counter += 1
 		        end		
 	        end		
 		end		
-
-        return alt_nums_list
 	end
-	
+
     def print_users_info
-    	puts "made it here"
-    	# puts @users_info
-    	@users_info.each do |user,user_list|
-    		# puts "#{user},#{user_list.join(",")}"
-    		print user
-    		user_list.each do |ele|
-    			if ele.is_a?(Array)
-    				print ",#{ele.join(",")}"
-    			else
-    				print ",#{ele}"
-    			end
-    		end
-    		print "\n"
-    	end
+    	puts "======================================================================================================"
+    	puts " notes: printing all alternate number slots, if altnum slot has phone number print, or else print extension, or print none if neither phone number or extension is available"
+    	puts "        print nothing if user is not assigned Alternate Numbers"
+    	puts "------------------------------------------------------------------------------------------------------"
+    	puts "Print User info for #{@ent}/#{@group}"
+    	puts "======================================================================================================"
+
+
+    	puts "UserId,LastName,FirstName,PhoneNumber,Extension,Alt1,Alt2,Alt3,Alt4,Alt5,Alt6,Alt7,Alt8,Alt9,Alt10"
+
+    	@users_info.each { |user,user_list| puts "#{user},#{user_list.join(",")}" }
     end
 
 end
