a0659c4c40ba6894d0c0faa85e80f471a6cc70be
Fabian, Peter <Peter.Fabian@cdk.com>
2016-05-05 22:06:29 -0700

    Add method to remove messaging from selected users

--- src/modifyUser.rb
+++ src/modifyUser.rb
@@ -12,13 +12,29 @@ class ModifyUser
 
 	def remove_receptionist(file_name)
 		rec_license = ["Client License 4"]
+		field_num = 2
 		user_list = Array.new
-		File.exist?(file_name) ? user_list = get_users_from_file(file_name) : user_list.push(file_name)
+		File.exist?(file_name) ? user_list = get_users_from_file(file_name,field_num) : user_list.push(file_name)
 		
 		user_list.each do |user|
 			
 			cmd_ok,response = $bw.mod_user_svc(user,rec_license)
 			puts "Deleted #{rec_license} from #{user}"
+		end
+
+	end
+
+	def remove_messaging(file_name)
+		vm_license = ['Voice Messaging User']
+		field_num = 0
+		user_list = Array.new
+		File.exist?(file_name) ? user_list = get_users_from_file(file_name,field_num) : user_list.push(file_name)
+		
+		user_list.each do |user|
+			puts "My user: #{user}"
+			
+			cmd_ok,response = $bw.mod_user_svc(user,vm_license)
+			puts "Deleted #{vm_license} from #{user}"
 		end
 
 	end
@@ -46,10 +62,10 @@ class ModifyUser
 	end
 
 
-    def get_users_from_file(file)
+    def get_users_from_file(file_name,field_num)
         user_list = Array.new
-        csv_file = CSV.read(file)
-        csv_file.each {|line| user_list.push(line[2])}
+        csv_file = CSV.read(file_name)
+        csv_file.each {|line| user_list.push(line[field_num])}
 
         return user_list
     end
