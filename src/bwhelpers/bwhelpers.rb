f8c3bdfb8252f22c6d23754e00c7051e910b06cf
Fabian, Peter <Peter.Fabian@cdk.com>
2016-05-27 11:26:32 -0700

    Add method to figure out what groups to query

--- src/bwhelpers/bwhelpers.rb
+++ src/bwhelpers/bwhelpers.rb
@@ -1,5 +1,18 @@
 
 class BWHelpers
 
+	def get_groups_to_query()
+		ent_groups = Hash.new(Array.new)
+		if $options.has_key?(:all_groups)
+			ent_groups = $bw.get_groups_in_system
+		elsif $options.has_key?(:group)
+			ent_groups = {$options[:ent] => [$options[:group]]}
+		else
+			abort "Please specify -g <GROUPID> or -a ALL"
+		end
+
+		return ent_groups
+	end	
+
 
 end
