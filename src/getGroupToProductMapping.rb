ba05e7baa7e7b461f066377e04180a3ef46931a3
Fabian, Peter <Peter.Fabian@cdk.com>
2016-05-27 11:28:45 -0700

    Get Group to Product Mapping in Prep for SNAP import

---
+++ src/getGroupToProductMapping.rb
@@ -0,0 +1,81 @@
+# TODO
+# PRINT OUTPUT per Transfer Data Spreadsheet from SNAP
+class GroupToProductMapping
+
+	def initialize(verbose)
+		@verbose = verbose
+		@domain_map = Hash.new
+		if ENV['IS_PROD'] == "true"
+			@domain_map = {
+				'as.s1.networkphoneasp.com' => 'Hosted',
+				's1.networkphoneasp.com' => 'Hosted',
+				'atc.networkphoneasp.com' => 'CTC',
+				'callconnect.s1.networkphoneasp.com' => 'CallConnect',
+				'ciscospa.s1.networkphoneasp.com' => 'SmallBusiness',
+			}
+		else
+			@domain_map = {
+				'as.lab1.adpvoice.com' => 'Hosted',
+				'as1' => 'Hosted',
+				'callconnect.lab1.adpvoice.com' => 'CallConnect',
+				'cisco.lab1.adpvoice.com' => 'CTC',
+				'cube.adpvoice.com' => 'CTC'
+			}
+		end	
+	end
+
+	def get_product_map(ent,group_list)
+		ent_name,ent_addr = get_profile_info(ent)
+		ent_info = {ent_id: ent, ent_name: ent_name, ent_addr: ent_addr, group_info: Hash.new}
+
+		group_list.each do |group|		
+			ent_info[:group_info] = Hash.new
+
+	        group_name,group_addr,product_type = get_profile_info(ent,group)
+	        ent_info[:group_info] = {group_id: group, group_name: group_name, group_addr: group_addr, product_type: product_type}
+
+			my_values = print_ent_info(ent_info)
+			counter = 0
+			my_values.each do |value|
+				print "\"#{value}\""
+				print "," unless counter == my_values.length - 1
+				print "\n" if counter == my_values.length - 1
+				counter += 1
+			end
+		end
+	end
+
+	def print_ent_info(ent_info,my_values=Array.new)
+		ent_info.each do |k,v|
+			print_ent_info(v,my_values) if v.is_a?(Hash)
+			my_values << v unless v.is_a?(Hash) || k == :country
+		end
+		return my_values
+	end
+
+	def get_profile_info(ent,group=nil)
+		name = nil
+		product_type = "UNKNOWN"		
+		addr = {addressLine1: nil, addressLine2: nil, city: nil, stateOrProvince: nil, zipOrPostalCode: nil, country: nil}
+
+		profile_info = nil
+		if group == nil
+			cmd_ok,profile_info = $bw.get_ent_profile(ent)
+			name = profile_info[:serviceProviderName]
+		else
+			cmd_ok,profile_info = $bw.get_group_profile(ent,group)
+			name = profile_info[:groupName] if profile_info.has_key?(:groupName)
+			product_type = get_default_domain(profile_info)
+		end
+
+		addr.merge!(profile_info[:address]) if @verbose && profile_info.has_key?(:address)
+		return name,addr,product_type
+	end
+
+	def get_default_domain(profile_info)
+		default_domain = profile_info[:defaultDomain]
+		product_type = @domain_map[default_domain] if @domain_map.has_key?(default_domain)
+	end
+
+
+end
