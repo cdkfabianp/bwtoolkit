114dbc32187cbdea329e6d7970e621ce5238b677
Fabian, Peter <Peter.Fabian@cdk.com>
2016-06-03 09:41:47 -0700

    .

--- src/auditMessaging.rb
+++ src/auditMessaging.rb
@@ -2,12 +2,13 @@
 class AuditMessaging
 
 	def initialize
-		@messaging_license = ["Voice Messaging User"]
+		@licenses = ["Voice Messaging User"]
 	end
 
 	def get_messaging_users(ent,group_list)
 		group_list.each do |group|
-			messaging_users = get_license_assignment(ent,group)
+			messaging_users = $bw_helper.get_license_assignment(ent,group,@licenses)
+			puts messaging_users
 		end
 
 
