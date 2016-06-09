b1f7a2324d8221543b8b1b60294a54a330ae8376
Fabian, Peter <Peter.Fabian@cdk.com>
2016-06-09 09:04:01 -0700

    Finalize getting user profile info

--- src/getUserProfile.rb
+++ src/getUserProfile.rb
@@ -3,7 +3,7 @@ class GetProfileInfo
 
 	def initialize(user,field_list)
 		@file_name = user
-		@field_list = get_field_list(field_list)
+		@field_list = field_list.split(",") if field_list
 
 	end
 
@@ -13,11 +13,20 @@ class GetProfileInfo
 		File.exist?(@file_name) ? user_list = $bw_helper.get_users_from_file(@file_name,field_num) : user_list.push(@file_name)
 		
 		user_list.each do |user|
-			puts "My user: #{user}"
+			cmd_ok,user_profile = $bw.get_user_filtered_info(user)
+			print_fields(user,user_profile)
 		end
 	end
 
-
-	def get_field_list(field_list)
+	def print_fields(user,profile)
+		if @field_list == nil
+			puts "#{user}: #{profile}"
+		elsif profile.length == 0
+			puts "#{user} profile not found"
+		else
+			profile.delete_if { |k| @field_list.include?(k.to_s) == false }
+			puts "#{user},#{profile.values.join(",")}"
+		end
 	end
+
 end
