fd396b15addae8212fe888698edac61b2207b827
Fabian, Peter <Peter.Fabian@cdk.com>
2016-06-13 15:43:49 -0700

    Migrate get user license into getuser profile

--- src/getUserProfile.rb
+++ src/getUserProfile.rb
@@ -1,32 +1,57 @@
 
 class GetProfileInfo
 
-	def initialize(user,field_list)
-		@file_name = user
-		@field_list = field_list.split(",") if field_list
+	def initialize
+	end
+
+	def get_user_info(sub_cmd)
+		send(sub_cmd)
 
 	end
 
 	def get_profile_info
-		field_num = 2
-		user_list = Array.new
-		File.exist?(@file_name) ? user_list = $bw_helper.get_users_from_file(@file_name,field_num) : user_list.push(@file_name)
+		file_name = $options[:file]
+		field_num = $options[:field_num]
+		fields = $options[:query].split(",") if $options[:query]
+		user_list = get_user_list(file_name,field_num.to_i)
 		
 		user_list.each do |user|
 			cmd_ok,user_profile = $bw.get_user_filtered_info(user)
-			print_fields(user,user_profile)
+
+			#Print Data
+			if fields == nil
+				puts "#{user}: #{user_profile}"
+			elsif user_profile.length == 0
+				puts "#{user} profile not found"
+			else
+				user_profile.delete_if { |k| fields.include?(k.to_s) == false }
+				puts "#{user},#{user_profile.values.join(",")}"
+			end			
 		end
 	end
 
-	def print_fields(user,profile)
-		if @field_list == nil
-			puts "#{user}: #{profile}"
-		elsif profile.length == 0
-			puts "#{user} profile not found"
-		else
-			profile.delete_if { |k| @field_list.include?(k.to_s) == false }
-			puts "#{user},#{profile.values.join(",")}"
+	def get_license_info
+		file_name = $options[:file]
+		field_num = $options[:field_num]
+		user_list = get_user_list(file_name,field_num.to_i)		
+		user_list.each do |user|
+			svc_list = $bw.get_user_clean_svc_list(user)
+			puts "#{user},#{svc_list}"
 		end
+		puts "#{user_list.length} records returned"
+	end
+
+	def get_user_list(file_name,field_num)
+		user_list = Array.new
+		if $options[:filter_user]
+			cmd_ok,user_list = $bw.get_users_in_sys_by_id({mode: "Contains", value: $options[:filter_user]})
+		elsif file_name
+			File.exist?(file_name) ? user_list = $bw_helper.get_users_from_file(file_name,field_num) : user_list.push(file_name)	
+		else
+			abort "Unable to get list of users"
+		end
+
+		return user_list
 	end
 
 end
