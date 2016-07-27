dd0c848754dda8b2e6930e31cd52d9814d11ec1f
Fabian, Peter <Peter.Fabian@cdk.com>
2016-07-27 13:11:06 -0700

    Depreciated script and moved to auditLicensePack.rb

--- src/auditStandard.rb
+++ src/auditStandard.rb
@@ -1,77 +1,71 @@
 
-class AuditStandard
+## Functionality of this script has been replaced by audit_service_pack (auditLicensePack.rb)
 
-	def initialize(audit_type,user_type,removals)
-		# @standard_list = [
-		# 	"Anonymous Call Rejection",
-		# 	"Automatic Callback",
-		# 	"Call Me Now",
-		# 	"Diversion Inhibitor",
-		# 	"Do Not Disturb",
-		# 	"Flexible Seating Guest",
-		# 	"Group Night Forwarding", 
-		# 	"Hoteling Guest",
-		# 	"Security Classification",
-		# 	"Speed Dial 100",
-		# 	"Speed Dial 8"
-		# ]
-		@standard_list = [
-			"Call Forwarding Always",
-			"Call Forwarding Busy",
-			"Call Forwarding No Answer",
-			"Call Forwarding Not Reachable"
-		]
-		puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-		puts "!!THIS SCRIPT IS CURRENTLY AUDITING VUPP1 Service PACK!!"
-		puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-		@removal_count = 0
-		@audit_type,@user_type,@removals,response = validate_input(audit_type,user_type,removals)
-		abort "Invalid input\n  #{response}" if response != nil
 
-		puts "TYPE: #{@audit_type}: USER_TYPE: #{@user_type}: REMOVALS: #{@removals}"
-	end
+# class AuditStandard
 
-	def get_standard_users(ent,group_list)
-		group_list.each do |group|
-			standard_users = $bw_helper.get_license_assignment(ent,group,@standard_list)
-			standard_users.each do |user,svc_list|
-				cmd_ok,user_addr_type = $bw.get_user_addr_type(user)
-				puts "AUDIT: #{ent},#{group},#{user},#{user_addr_type},#{svc_list}"
+# 	def initialize(audit_type,user_type,removals)
+# 		@standard_list = [
+# 			"Anonymous Call Rejection",
+# 			"Automatic Callback",
+# 			"Call Me Now",
+# 			"Diversion Inhibitor",
+# 			"Do Not Disturb",
+# 			"Flexible Seating Guest",
+# 			"Group Night Forwarding", 
+# 			"Hoteling Guest",
+# 			"Security Classification",
+# 			"Speed Dial 100",
+# 			"Speed Dial 8"
+# 		]
+# 		@removal_count = 0
+# 		@audit_type,@user_type,@removals,response = validate_input(audit_type,user_type,removals)
+# 		abort "Invalid input\n  #{response}" if response != nil
 
-				#Reclaim Licenses if Task = recover
-				if @removal_count <= @removals
-					reclaim_licenses(user,svc_list) if @ok_to_delete == true && user_addr_type == @user_type
-				else
-					abort "Reached Maximum number of Removals: #{@removal_count}"
-				end
-			end
-		end
-	end
+# 		puts "TYPE: #{@audit_type}: USER_TYPE: #{@user_type}: REMOVALS: #{@removals}"
+# 	end
 
-	def reclaim_licenses(user,svc_list)
-		cmd_ok,response = $bw.mod_user_svc(user,svc_list)
-		puts "REMOVED: #{user}, #{svc_list}, OK: #{cmd_ok}"
-		@removal_count += 1
+# 	def get_standard_users(ent,group_list)
+# 		group_list.each do |group|
+# 			standard_users = $bw_helper.get_license_assignment(ent,group,@standard_list)
+# 			standard_users.each do |user,svc_list|
+# 				cmd_ok,user_addr_type = $bw.get_user_addr_type(user)
+# 				puts "AUDIT: #{ent},#{group},#{user},#{user_addr_type},#{svc_list}"
 
-	end
+# 				#Reclaim Licenses if Task = recover
+# 				if @removal_count <= @removals
+# 					reclaim_licenses(user,svc_list) if @ok_to_delete == true && user_addr_type == @user_type
+# 				else
+# 					abort "Reached Maximum number of Removals: #{@removal_count}"
+# 				end
+# 			end
+# 		end
+# 	end
 
-	def validate_input(audit_type,user_type,removals)
-		response = nil
+# 	def reclaim_licenses(user,svc_list)
+# 		cmd_ok,response = $bw.mod_user_svc(user,svc_list)
+# 		puts "REMOVED: #{user}, #{svc_list}, OK: #{cmd_ok}"
+# 		@removal_count += 1
 
-		#Validate Audit Type (Will Default to audit)
-		audit_type = "audit" unless audit_type == "recover"
+# 	end
+
+# 	def validate_input(audit_type,user_type,removals)
+# 		response = nil
+
+# 		#Validate Audit Type (Will Default to audit)
+# 		audit_type = "audit" unless audit_type == "recover"
 		
-		#Validate User Type
+# 		#Validate User Type
 
-		# Moved to BW_Helpers to populate Array of valid user types
-		# valid_user_types = ["Hosted_User", "Trunk_User", "Virtual_User", "None"]		
-		response = "Invalid User Type: (#{user_type})\n  Valid options are #{$bw_helper.valid_user_types}" if audit_type == "recover" && $bw_helper.valid_user_types.include?(user_type) == false
+# 		# Moved to BW_Helpers to populate Array of valid user types
+# 		# valid_user_types = ["Hosted_User", "Trunk_User", "Virtual_User", "None"]		
+# 		response = "Invalid User Type: (#{user_type})\n  Valid options are #{$bw_helper.valid_user_types}" if audit_type == "recover" && $bw_helper.valid_user_types.include?(user_type) == false
 
-		#Convert Removals to Integer
-		new_removals = 0
-		new_removals = removals.to_i if removals != nil
+# 		#Convert Removals to Integer
+# 		new_removals = 0
+# 		new_removals = removals.to_i if removals != nil
 
-		return audit_type,user_type,new_removals,response
+# 		return audit_type,user_type,new_removals,response
 
-	end
-end
+# 	end
+# end
