d2088d0454ef4fd238ef2679a39b13a1d0a59db7
Fabian, Peter <Peter.Fabian@cdk.com>
2016-08-09 08:42:42 -0700

    Modify output to query Marchex info

--- src/tn_search.rb
+++ src/tn_search.rb
@@ -26,34 +26,47 @@ PARAGRAPH
         e164_number = false
         invalid_input = false
         extra_digits = ""
-        puts "searching for (#{tn})"
+        # puts "searching for (#{tn})"
         if tn =~ /(^[2-9]\d{9})(\d*)/ #|| tn =~ /^1([2-9]\d{9})(\d*)/
             e164_number = "+1#{$1}"
             extra_digits = $2
         elsif /^\+1([2-9]\d{9})(\d*)/ =~ tn
-            puts "Matched +1"
+            # puts "Matched +1"
             e164_number = "+1#{$1}"
             extra_digits = $2
         end
-        puts "found e164: #{e164_number}"
+        # puts "found e164: #{e164_number}"
         return e164_number
     end
 
-    def tn_search(tn_number)
-        tns = Array.new
-        tns << tn_number
-        tns.each do |tn|
+    def tn_search(tns)
+        # tns = Array.new
+        # tns << tn_number
+        tns.each do |tn|  
             e164_number = normalize_tn(tn)
             if e164_number
                 cmd_ok,response_hash = $bw.find_tn_assignment(e164_number)
                 response_hash = {} if cmd_ok == false || e164_number == false
-                puts "#{response_hash}"
+                # puts "#{response_hash}"
+                puts print_marchex_info(tn,response_hash)
             else
-                puts "Could not get e164 number from: #{tn}"
+                puts "#{tn},invalid number format"
             end
         end
-
     end
+
+    def print_marchex_info(t,info)
+        print_string = "#{t},"
+        if t.length > 0
+            print_string += "#{info[:serviceProviderId]},#{info[:groupId]},#{info[:isActivated]}"
+        else
+            print_string += "NOT_FOUND"
+        end
+
+        return print_string
+    end
+
+
 
     def group_tn_list(ent,group)
         cmd_ok,response_hash = $bw.get_group_dn_list(ent,group)
