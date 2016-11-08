e26b5afb7817d83db6756e68ca90318d36c96b52
Fabian, Peter <Peter.Fabian@cdk.com>
2016-11-07 20:31:26 -0800

    major updates to get_reg_info, use version info from dev_info instead of querying user reg status

--- src/getRegistrationByDeviceType.rb
+++ src/getRegistrationByDeviceType.rb
@@ -12,15 +12,26 @@ class GetRegByDeviceType
 	end
 
 	def get_poly_list
-		puts @counts == true ? "Enterprise,Group,Configured,Registered" : "Enterprise,Group,Group Name,DeviceType,DeviceVersion,DeviceConfigType,DeviceMac"
+		puts @counts == true ? "Enterprise,Group,Configured,Registered" : "\"Enterprise\",\"Group - Group Name\",\"ConfiguredDeviceType\",\"ActualDeviceType\",\"DeviceVersion\",\"DeviceConfigType\",\"DeviceMac\""
 		@ent_groups.each do |ent,groups|
 			groups.each do |group|
 				cmd_ok,g_profile = $bw.get_group_profile(ent,group)
-			 	g_profile.has_key?(:groupName) ? group_name = g_profile[:groupName] : group_name = "__NONE__"
+
+				# Get Group Name (Remove surrounding quotes as I add that back in when I do the final print)
+				# If Group Name doesn't exist add __NONE__
+			 	group_name = g_profile.has_key?(:groupName) ? g_profile[:groupName].gsub(/\"(.*)\"/, '\1') : "__NONE__"
+
+			 	# Get List of Devices for group
 				devices_list = $bw.get_group_device_list_by_type(ent,group,@device_search_list)
+
+				#For each device get UA (if registered), and other device info
 				ua_device_list = get_reg_info(ent,group,devices_list)
+
+				#Get count of devices both deg and configured
 				configed_devices = devices_list.length
 				reged_devices = ua_device_list.length
+
+				#Print results, if $options[:counts] (IE -s true) print only summary information, otherwise print details
 				@counts == true ? print_poly_reg_counts(ent,group,group_name,configed_devices,reged_devices) : print_poly_reg_list(ent,group,group_name,ua_device_list)
 			end
 		end
@@ -33,7 +44,7 @@ class GetRegByDeviceType
 	end
 
 	def print_poly_reg_list(ent,group,group_name,ua_device_list)
-		ua_device_list.each { |dev_mac,dev_info| puts "#{ent}|#{group} - #{group_name}|#{dev_info.join("|")}|#{dev_mac}" }		
+		ua_device_list.each { |dev_mac,dev_info| puts "\"#{ent}\"|\"#{group} - #{group_name}\"|\"#{dev_info.join("\"|\"")}\"|\"#{dev_mac}\"" }		
 	end
 
 	def get_reg_info(ent,group,devices_list)
@@ -42,42 +53,48 @@ class GetRegByDeviceType
 	    devices_list.each do |device_list|
 	    	next if device_list[:MAC_Address] == "__NIL__"
 	    	dev_name = device_list[:Device_Name]
-	    	config_type = get_phone_config_info(ent,group,dev_name)
 
-				cmd_ok, user_ids = $bw.get_users_assigned_to_device(ent,group,dev_name)
-				user_ids.each do |user|
-					#Skip User Reg Lookup if we have already found the Device Info
-					next if ua_device_list.has_key?(device_list[:MAC_Address])
+	    	#config_dev_type is the Configured Identity/Device Profile (dev_type is the actual device model from User-Agent)
+	    	config_type,config_dev_type,version = get_phone_config_info(ent,group,dev_name)
+	    	if version
+	    		dev_type,dev_ver,dev_mac = parse_ua(version)
+	    		dev_mac = device_list[:MAC_Address] unless dev_mac
+	    		ua_device_list[dev_mac] = [config_dev_type,dev_type,dev_ver,config_type]
+	    	end
+				# cmd_ok, user_ids = $bw.get_users_assigned_to_device(ent,group,dev_name)
+				# user_ids.each do |user|
+				# 	#Skip User Reg Lookup if we have already found the Device Info
+				# 	next if ua_device_list.has_key?(device_list[:MAC_Address])
 
-					cmd_ok,user_reg = $bw.get_user_register_status(user)
-					user_reg.each do |line_reg|
-							dev_type,dev_ver,dev_mac = parse_ua(line_reg[:User_Agent])
-
-							#Insert configured MAC if MAC doesn't exist within User-Agent string
-							dev_mac = device_list[:MAC_Address] unless dev_mac
-							ua_device_list[dev_mac] = [dev_type,dev_ver,config_type]
-					end
-				end
+				# 	cmd_ok,user_reg = $bw.get_user_register_status(user)
+				# 	user_reg.each do |line_reg|
+				# 			dev_type,dev_ver,dev_mac = parse_ua(line_reg[:User_Agent])
+				# 			puts ""
+				# 			#Insert configured MAC if MAC doesn't exist within User-Agent string
+				# 			dev_mac = device_list[:MAC_Address] unless dev_mac
+				# 			ua_device_list[dev_mac] = [dev_type,dev_ver,config_type,config_dev_type]
+				# 	end
+				# end
 	    end
 
 	    return ua_device_list
 	end
 
 	def get_phone_config_info(ent,group,dev_name)
+		# dev_info[:version] now shows registered User-Agent
 		cmd_ok,dev_info = $bw.get_user_device_info(ent,group,dev_name)
-		return dev_info[:configurationMode]
+		return dev_info[:configurationMode],dev_info[:deviceType],dev_info[:version]
 	end
 
 	def parse_ua(ua)
 		dev_type = "UNKNOWN DEVICE USER_AGENT (#{ua})"
 		dev_ver = "unknown"
 		dev_mac = "unknown"
-		/^(Polycom.*)-UA\/([\.\d]+)/.match(ua)
+		/(Polycom[VS].+)-UA\/([\.\d]+)/.match(ua)
 			dev_type = $1
 			dev_ver = $2
 		/_(\w{12})$/.match(ua)		
 			dev_mac = $3
-
 		return dev_type,dev_ver,dev_mac
 	end	
 
